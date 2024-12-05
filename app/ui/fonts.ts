d17443f455972c8dc48deb7486ef47759809cafa
shereef <shiref.hamam2@gmail.com>
2024-12-05 22:40:18 +0200

    feat(next): add image and fonts in next js

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
